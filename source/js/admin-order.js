27f071d1c04de49e546cfc149798a05fbbbccafe
trnet4334 <search0210@gmail.com>
2018-08-24 11:15:01 -0700

    Fix the problem of JQuery function

--- source/js/admin-order.js
+++ source/js/admin-order.js
@@ -1,15 +1,18 @@
 $(function(){
+
   //menu toogle
   const menuItem = $('#navbar .nav-item');
   const mainContent = $('.js_mainContent');
+
   menuItem.on('click', function(e){
     e.preventDefault();
     menuItem.removeClass('active');
     $(this).addClass('active');
     const target = $(this).find('a').attr('href');
     mainContent.not(target).hide().addClass('d-none');
-    $(target).fadeIn().removeClass('d-none');
+    $(target).show().removeClass('d-none');
   });
+
   //c3 chart
   const chart = c3.generate({
     bindto: '#chart',
